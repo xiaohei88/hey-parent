500dbcf447e45f6a70e8391ac0f971c526c0e0a9
helei <he.lei45@zte.com.cn>
2017-07-20 10:54:54 +0800

    update

--- hey-common/src/main/java/org/heyframework/common/exception/handler/ExceptionHandler.java
+++ hey-common/src/main/java/org/heyframework/common/exception/handler/ExceptionHandler.java
@@ -16,86 +16,88 @@ import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 public class ExceptionHandler extends SimpleMappingExceptionResolver {
 
-	private String errorPage;
+    private String errorPage;
 
-	@Override
-	public ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object object, Exception exception) {
+    @Override
+    public ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object object, Exception exception) {
 
-		// 1、根据不同的异常获取不同的消息
-		Map<String, Object> map = _getMessage(exception);
+        // 1、根据不同的异常获取不同的消息
+        Map<String, Object> map = _getMessage(exception);
 
-		String accept = request.getHeader("Accept");
-		if (!StringUtils.isEmpty(accept) && (!accept.startsWith("text/html") || "ios".equals(accept))) {
-			// 2、处理ajax请求
-			_out(response, map);
-			return null;
-		} else {
-			String uri = request.getRequestURI();
-			ModelAndView mv = new ModelAndView();
-			mv.addAllObjects(map);
-			/** .h=.html;.j=*.jsp;.f=.form **/
-			if (accept.startsWith("text/html, */*") || uri.endsWith(".h") || uri.endsWith("*.j") || uri.endsWith(".f")) {
-				// 3、处理页面请求
-				mv.setViewName(errorPage);
-			} else {
-				// 4、处理form请求
-				mv.setViewName("errorJson");
-			}
-			return mv;
-		}
-	}
+        String accept = request.getHeader("Accept");
+        if (!StringUtils.isEmpty(accept) && (!accept.startsWith("text/html") || "ios".equals(accept))) {
+            // 2、处理ajax请求
+            _out(response, map);
+            return null;
+        } else {
+            String uri = request.getRequestURI();
+            ModelAndView mv = new ModelAndView();
+            mv.addAllObjects(map);
+            /** .h=.html;.j=*.jsp;.f=.form **/
+            if (accept.startsWith("text/html, */*") || uri.endsWith(".h") || uri.endsWith("*.j") || uri.endsWith(".f")) {
+                // 3、处理页面请求
+                mv.setViewName(errorPage);
+            } else {
+                // 4、处理form请求
+                mv.setViewName("errorJson");
+            }
+            return mv;
+        }
+    }
 
-	/**
-	 * 根据不同的异常获取不同的信息
-	 * 
-	 * @param exception
-	 * @return
-	 */
-	private Map<String, Object> _getMessage(Exception exception) {
-		Map<String, Object> map = new HashMap<String, Object>(2);
-		int code = 0;
-		String msg = "UNKNOWN ERROR.";
-		if (exception instanceof ClassNotFoundException) {
-			msg = "can't find this class " + exception.getMessage() + ".";
-		} else if (exception instanceof NullPointerException) {
-			msg = "the method has null pointer.";
-		} else if (exception instanceof DataIntegrityViolationException) {
-			msg = exception.getCause().getMessage();
-		} else {
-			msg = exception.getMessage();
-		}
+    /**
+     * 根据不同的异常获取不同的信息
+     *
+     * @param exception
+     * @return
+     */
+    private Map<String, Object> _getMessage(Exception exception) {
+        Map<String, Object> map = new HashMap<String, Object>(2);
+        int code = 0;
+        String msg = "UNKNOWN ERROR.";
+        if (exception instanceof ClassNotFoundException) {
+            msg = "can't find this class " + exception.getMessage() + ".";
+        } else if (exception instanceof NullPointerException) {
+            msg = "the method has null pointer.";
+        } else if (exception instanceof DataIntegrityViolationException) {
+            msg = exception.getCause().getMessage();
+        } else {
+            msg = exception.getMessage();
+        }
 
-		// 去除换行的问题
-		msg = msg.replaceAll("\r|\n", "");
-		// 去除双引号的问题
-		msg = msg.replace("\"", "\\\"");
-		map.put("code", code);
-		map.put("msg", "reason:" + msg);
-		return map;
-	}
+        if (!StringUtils.isEmpty(msg)) {
+            // 去除换行的问题
+            msg = msg.replaceAll("\r|\n", "");
+            // 去除双引号的问题
+            msg = msg.replace("\"", "\\\"");
+        }
+        map.put("code", code);
+        map.put("msg", "reason:" + msg);
+        return map;
+    }
 
-	/**
-	 * 错误信息输出
-	 * 
-	 * @param response
-	 * @param map
-	 */
-	private void _out(HttpServletResponse response, Map<String, Object> map) {
-		response.setHeader("Content-Type", "application/json;charset=UTF-8");
-		PrintWriter pw = null;
-		try {
-			pw = response.getWriter();
-			JsonUtils.writeValue(pw, map);
-		} catch (IOException e) {
-			logger.error(e);
-		}
-	}
+    /**
+     * 错误信息输出
+     *
+     * @param response
+     * @param map
+     */
+    private void _out(HttpServletResponse response, Map<String, Object> map) {
+        response.setHeader("Content-Type", "application/json;charset=UTF-8");
+        PrintWriter pw = null;
+        try {
+            pw = response.getWriter();
+            JsonUtils.writeValue(pw, map);
+        } catch (IOException e) {
+            logger.error(e);
+        }
+    }
 
-	public String getErrorPage() {
-		return errorPage;
-	}
+    public String getErrorPage() {
+        return errorPage;
+    }
 
-	public void setErrorPage(String errorPage) {
-		this.errorPage = errorPage;
-	}
+    public void setErrorPage(String errorPage) {
+        this.errorPage = errorPage;
+    }
 }
